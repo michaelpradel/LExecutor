fe6c25100dc736335c6d34c6d20a00b74ec3fb6d
Michael Pradel <michael@binaervarianz.de>
2022-02-11 11:58:19 +0100

    bug fix

--- src/lexecutor/Model.py
+++ src/lexecutor/Model.py
@@ -12,7 +12,7 @@ class ValuePredictionModel(nn.Module):
             in_features=4+p.token_emb_len+p.max_call_args*p.value_emb_len+3*p.value_emb_len+p.token_emb_len, out_features=p.intermediate_layer_len)
         self.final_fc = nn.Linear(
             in_features=p.intermediate_layer_len, out_features=p.value_emb_len)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, xs):
         xs_kind, xs_name, xs_args, xs_base, xs_left, xs_right, xs_operator = xs
@@ -23,6 +23,6 @@ class ValuePredictionModel(nn.Module):
                             xs_right, xs_operator), dim=1)
 
         intermediate_out = F.relu(self.intermediate_fc(all_joined))
-        final_out = self.softmax(self.final_fc(intermediate_out), dim=1)
+        final_out = self.softmax(self.final_fc(intermediate_out))
 
         return final_out
