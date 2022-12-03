f6b6d6a588cbce3c8890f86357a8de042503a137
Michael Pradel <michael@binaervarianz.de>
2022-12-03 12:48:47 +0100

    Adapting predictor to newest model

--- src/lexecutor/predictors/codet5/CodeT5ValuePredictor.py
+++ src/lexecutor/predictors/codet5/CodeT5ValuePredictor.py
@@ -6,15 +6,15 @@ from .FineTune import load_CodeT5
 from .InputFactory import InputFactory
 from ...ValueAbstraction import restore_value
 from ...IIDs import IIDs
+from ...Hyperparams import Hyperparams as params
 
 
 class CodeT5ValuePredictor(ValuePredictor):
     def __init__(self):
-
         # load model
         self.tokenizer, self.model = load_CodeT5()
         self.model.load_state_dict(t.load(
-            "data/codeT5_models/checkpoint-last/pytorch_model_epoch_1_without_name-value-duplicates.bin", map_location=device))
+            "data/codeT5_models/dec2_91perc_val_acc/pytorch_model.bin", map_location=device))
 
         self.iids = IIDs('iids_original.json')
 
@@ -27,14 +27,14 @@ class CodeT5ValuePredictor(ValuePredictor):
         with t.no_grad():
             self.model.eval()
             generated_ids = self.model.generate(
-                t.tensor(np.array([input_ids]), device=device), max_length=7)
+                t.tensor(np.array([input_ids]), device=device), max_length=params.max_output_length)
 
         predicted_value = self.tokenizer.decode(
-            generated_ids[0][5], skip_special_tokens=True)
+            generated_ids[0], skip_special_tokens=True)
         return restore_value(predicted_value)
 
     def name(self, iid, name):
-        entry = {"iid": iid, "name": name}
+        entry = {"iid": iid, "name": name, "kind": "name"}
         v = self.__query_model(entry)
         print(f"{iid}: Predicting for name {name}: {v}")
         return v
@@ -43,13 +43,13 @@ class CodeT5ValuePredictor(ValuePredictor):
         fct_name = fct.__name__ if hasattr(fct, "__name__") else str(fct)
         if " " in fct_name:  # some fcts that don't have a proper name
             fct_name = fct_name.split(" ")[0]
-        entry = {"iid": iid, "name": fct_name}
+        entry = {"iid": iid, "name": fct_name, "kind": "call"}
         v = self.__query_model(entry)
         print(f"{iid}: Predicting for call: {v}")
         return v
 
     def attribute(self, iid, base, attr_name):
-        entry = {"iid": iid, "name": attr_name}
+        entry = {"iid": iid, "name": attr_name, "kind": "attribute"}
         v = self.__query_model(entry)
         print(f"{iid}: Predicting for attribute {attr_name}: {v}")
         return v
