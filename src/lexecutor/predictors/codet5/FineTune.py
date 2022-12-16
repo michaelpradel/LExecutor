768420f834927eb07a84a8be6cb9d8669a85ef92
Michael Pradel <michael@binaervarianz.de>
2022-12-16 15:34:02 +0100

    Preparing for longer training:  * Log both train loss and val. acc.  * Save model after each epoch  * Store val. examples in human-readable format

--- src/lexecutor/predictors/codet5/FineTune.py
+++ src/lexecutor/predictors/codet5/FineTune.py
@@ -20,8 +20,6 @@ parser.add_argument(
     "--validate_tensors", help=".pt files for validation", default="validate.pt")
 parser.add_argument(
     "--output_dir", help="directory to store models", required=True)
-parser.add_argument(
-    "--save_last_checkpoints", help="Save checkpoint after every 100 batches", action="store_true")
 
 
 print_examples = True
@@ -38,6 +36,9 @@ def evaluate(validate_tensors_path, model, tokenizer):
     logger.info("  Batch size = {}".format(params.batch_size))
 
     accuracies = []
+    all_inputs = []
+    all_labels = []
+    all_predictions = []
 
     with t.no_grad():
         model.eval()
@@ -63,6 +64,10 @@ def evaluate(validate_tensors_path, model, tokenizer):
             accuracies.append(accuracies_batch)
 
             # for debugging
+            all_inputs.extend(tokenizer.batch_decode(
+                input_ids, skip_special_tokens=True))
+            all_labels.extend(labels)
+            all_predictions.extend(predictions)
             if print_examples:
                 for label_idx, label in enumerate(labels):
                     if random.uniform(0, 100) < 0.1:
@@ -74,12 +79,19 @@ def evaluate(validate_tensors_path, model, tokenizer):
     logger.info(
         f"val_accuracy = {round(val_accuracy, 4)}")
 
-    logger.info('Terminating evaluation')
+    logger.info("Storing examples in human-readable format")
+    examples_df = pd.DataFrame(
+        {"input": all_inputs, "label": all_labels, "prediction": all_predictions})
+    examples_df.to_pickle("./eval_examples.pkl")
+
+    logger.info('Done with evaluation')
+    return val_accuracy
 
 
-def save_model(model, output_dir):
+def save_model(model, output_dir, epoch):
     model_to_save = model.module if hasattr(model, 'module') else model
-    output_model_file = os.path.join(output_dir, "pytorch_model.bin")
+    output_model_file = os.path.join(
+        output_dir, f"pytorch_model_epoch{epoch}.bin")
     t.save(model_to_save.state_dict(), output_model_file)
     logger.info("Saved the last model into %s", output_model_file)
 
@@ -102,9 +114,11 @@ if __name__ == "__main__":
         len(train_dataset) / params.batch_size))
     logger.info("  Num epoch = {}".format(params.epochs))
 
-    output_dir = os.path.join(args.output_dir, 'checkpoint-last')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    df_training_loss = pd.DataFrame(columns=['batch', 'loss', 'epoch'])
+    df_validation_acc = pd.DataFrame(columns=['epoch', 'val_accuracy'])
 
     for epoch in range(params.epochs):
         logger.info(f"Epoch {epoch}")
@@ -128,29 +142,23 @@ if __name__ == "__main__":
             logger.info(
                 f"  Training loss of batch {batch_idx}: {round(loss.item(), 4)}")
 
-            if (batch_idx+1) % 100 == 0 and args.save_last_checkpoints:
-                save_model(model, output_dir)
-                evaluate(args.validate_tensors, model, tokenizer)
-
-            # save last training loss
-            if not os.path.isfile(f'./training_loss.csv'):
-                columns = ['batch', 'loss']
-
-                with open(f'./training_loss.csv', 'a') as csvFile:
-                    writer = csv.writer(csvFile)
-                    writer.writerow(columns)
-
-            df = pd.read_csv(f'./training_loss.csv')
-            df_new_data = pd.DataFrame({
+            # save training losses to file
+            df_training_loss = pd.concat([df_training_loss, pd.DataFrame({
                 'batch': [batch_idx],
                 'loss': [round(loss.item(), 4)],
                 'epoch': [epoch]
-            })
-            df = pd.concat([df, df_new_data])
-            df.to_csv(f'./training_loss.csv', index=False)
+            })])
+            df_training_loss.to_csv('./training_loss.csv', index=False)
 
-        evaluate(args.validate_tensors, model, tokenizer)
+        val_accuracy = evaluate(args.validate_tensors, model, tokenizer)
 
-    save_model(model, output_dir)
+        # save validation accuracies to file
+        df_validation_acc = pd.concat([df_validation_acc, pd.DataFrame({
+            "epoch": [epoch],
+            "val_accuracy": [val_accuracy]
+        })])
+        df_validation_acc.to_csv('./validation_acc.csv', index=False)
+
+        save_model(model, args.output_dir, epoch)
 
     logger.info('Terminating training')
