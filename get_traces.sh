e02135ce2fe3bc5ab61bd842fa13d617da0d55a1
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2023-01-06 00:13:28 +0100

    Add initial script to get traces automatically

---
+++ get_traces.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# $1: repo URL
+# $2: directory to instrument in repository
+# $3: test directory in repository
+
+REPO_NAME=$(echo $1 | grep -o '[^/]*$')
+
+# Download repo
+git -C ./data/repos clone $1 
+
+# Install requirements
+cd ./data/repos/$REPO_NAME
+python3 setup.py install
+
+# Install additional requirements
+# Rich
+pip install commonmark
+pip install pygments
+# Requests
+pip install trustme
+
+# Instrument
+cd ../../../
+
+FILES_TO_INSTRUMENT=$(find ./data/repos/$REPO_NAME/$2 -type f -name "*.py")
+
+python3 -m lexecutor.Instrument --files $FILES_TO_INSTRUMENT --iids iids.json
+
+# Discard tests that cannot be executed
+# Request
+# rm ./data/repos/$REPO_NAME/$3/conftest.py
+# Run tests
+pytest ./data/repos/$REPO_NAME/$3
