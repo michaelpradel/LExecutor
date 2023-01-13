af8455cc369fec5466363b8ffa976ef50671a418
Michael Pradel <michael@binaervarianz.de>
2023-01-13 11:37:03 +0100

    Commiting uninstrumented files instead of instrumented ones

--- tests/broken_commit_new.py
+++ tests/broken_commit_new.py
@@ -1,14 +1,9 @@
-# LExecutor: DO NOT INSTRUMENT
+if (not has_min_size(all_data)):
+    raise RuntimeError("not enough data")
 
-from lexecutor.Runtime import _n_
-from lexecutor.Runtime import _a_
-from lexecutor.Runtime import _c_
-if (not _c_(770542, _n_(770540, "has_min_size", lambda: has_min_size), _n_(770541, "all_data", lambda: all_data))):
-    raise _c_(770544, _n_(770543, "RuntimeError", lambda: RuntimeError), "not enough data")
+train_len = 0.8 * len(all_data)
 
-train_len = 0.8 * _c_(770547, _n_(770545, "len", lambda: len), _n_(770546, "all_data", lambda: all_data))
+logger.info(f"Extracting training data with config {config_str}")
 
-_c_(770551, _a_(770549, _n_(770548, "logger", lambda: logger), "info"), f"Extracting training data with config {_n_(770550, 'config_str', lambda: config_str)}")
-
-train_data = _n_(770552, "all_data", lambda: all_data)[0:_n_(770553, "train_len", lambda: train_len)]
-_c_(770556, _n_(770554, "print", lambda: print), _n_(770555, "train_data", lambda: train_data))
+train_data = all_data[0:train_len]
+print(train_data)
--- tests/broken_commit_old.py
+++ tests/broken_commit_old.py
@@ -1,14 +1,9 @@
-# LExecutor: DO NOT INSTRUMENT
+if (not has_min_size(all_data)):
+    raise RuntimeError("not enough data")
 
-from lexecutor.Runtime import _n_
-from lexecutor.Runtime import _a_
-from lexecutor.Runtime import _c_
-if (not _c_(770559, _n_(770557, "has_min_size", lambda: has_min_size), _n_(770558, "all_data", lambda: all_data))):
-    raise _c_(770561, _n_(770560, "RuntimeError", lambda: RuntimeError), "not enough data")
+train_len = round(0.8 * len(all_data))
 
-train_len = _c_(770566, _n_(770562, "round", lambda: round), 0.8 * _c_(770565, _n_(770563, "len", lambda: len), _n_(770564, "all_data", lambda: all_data)))
+logger.info(f"Extracting training data with config {config_str}")
 
-_c_(770570, _a_(770568, _n_(770567, "logger", lambda: logger), "info"), f"Extracting training data with config {_n_(770569, 'config_str', lambda: config_str)}")
-
-train_data = _n_(770571, "all_data", lambda: all_data)[0:_n_(770572, "train_len", lambda: train_len)]
-_c_(770575, _n_(770573, "print", lambda: print), _n_(770574, "train_data", lambda: train_data))
+train_data = all_data[0:train_len]
+print(train_data)
