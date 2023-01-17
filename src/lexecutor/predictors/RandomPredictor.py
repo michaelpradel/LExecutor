73171c5d906be07b8a48630d33b7151dd19033dc
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2023-01-17 01:25:34 +0100

    Add purely random predictor

---
+++ src/lexecutor/predictors/RandomPredictor.py
@@ -0,0 +1,51 @@
+from .ValuePredictor import ValuePredictor
+from ..ValueAbstraction import DummyObject, DummyResource
+from ..Logging import logger
+import random
+
+class RandomPredictor(ValuePredictor):
+    def __init__(self):
+        super().__init__()
+        self.values = [
+            None,
+            True,
+            False,
+            "",
+            "a",
+            -1,
+            0,
+            1,
+            -1.0,
+            0.0,
+            1.0,
+            [],
+            [DummyObject()],
+            (),
+            (DummyObject(),),
+            set(),
+            {DummyObject()},
+            {},
+            {"a": DummyObject()},
+            DummyResource(),
+            DummyObject,
+            DummyObject()
+        ]
+        
+    def get_random_value(self):
+        random_index = random.randint(0, len(self.values) - 1)
+        return self.values[random_index]
+    
+    def name(self, iid, name):
+        v = self.get_random_value()
+        logger.info(f"{iid}: Predicting for name {name}: {v}")
+        return v
+
+    def call(self, iid, fct, *args, **kwargs):
+        v = self.get_random_value()
+        logger.info(f"{iid}: Predicting for call: {v}")
+        return v
+
+    def attribute(self, iid, base, attr_name):
+        v = self.get_random_value()
+        logger.info(f"{iid}: Predicting for attribute {attr_name}: {v}")
+        return v
