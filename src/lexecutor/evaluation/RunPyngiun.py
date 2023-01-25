4d2c73e16b50cd131bd1c444a545402c1d510d09
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2023-01-25 12:40:14 +0100

    Fix module pynguin path

--- src/lexecutor/evaluation/RunPyngiun.py
+++ src/lexecutor/evaluation/RunPyngiun.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     files = gather_files(args.files)
     os.environ['PYNGUIN_DANGER_AWARE'] = 'x'
-
+    
     pynguin_parameters = '--maximum_search_time 20 --seed 42 --max-attempts 10 --maximum_test_execution_timeout 10 --maximum_slicing_time 10 --test_execution_time_per_statement 1 --assertion-generation SIMPLE -v'
 
     for file in files:
         if file.startswith(os.getcwd()):
             file = file[len(os.getcwd())+1:]
-        module_pynguin_path = file.replace("/", ".")[:-3]
+        module_pynguin_path = file.replace("/", ".")[2:-3]
         print(f"Running Pynguin on {module_pynguin_path} with parameters {pynguin_parameters}")
         log_pynguin = subprocess.run(f"pynguin --project-path . --output-path ./tmp --module-name {module_pynguin_path} {pynguin_parameters}".split(
             " "), capture_output=True, text=True, shell=False, timeout=60)
