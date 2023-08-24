046674cf19e0faadaffdedd456704ac7a98ad19e
Beatriz <beatriz.souza@ccc.ufcg.edu.br>
2023-08-24 14:49:51 +0200

    Add args to EvaluateModels.py

--- src/lexecutor/evaluation/EvaluateModels.py
+++ src/lexecutor/evaluation/EvaluateModels.py
@@ -1,23 +1,38 @@
+import argparse
 import torch as t
 from ..predictors.DLUtil import device
-from ..predictors.codet5.FineTune import load_CodeT5, evaluate
+from ..predictors.codet5.FineTune import evaluate as evaluate_CodeT5, load_CodeT5
+from ..predictors.codebert.FineTune import evaluate as evaluate_CodeBERT, load_CodeBERT
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model_type", help="CodeT5 or CodeBERT", required=True)
+parser.add_argument(
+    "--value_abstraction", help="fine-grained or coarse-grained", required=True)
+parser.add_argument(
+    "--state_file", help=".bin file for validation", default="model.bin")
+parser.add_argument(
+    "--validate_tensors", help=".pt file for validation", default="validate.pt")
 
-def evaluate_model(name, state_file, validation_data_file):
-    tokenizer, model = load_CodeT5()
-    model.load_state_dict(t.load(state_file, map_location=device))
-    topk_accuracies = evaluate(validation_data_file, model, tokenizer)
+def evaluate_model(model_type, value_abstraction, state_file, validation_data_file):
+    if model_type == "CodeT5":
+        tokenizer, model = load_CodeT5()
+        model.load_state_dict(t.load(state_file, map_location=device))
+        topk_accuracies = evaluate_CodeT5(validation_data_file, model, tokenizer)
+    else:
+        tokenizer, model = load_CodeBERT()
+        model.load_state_dict(t.load(state_file, map_location=device))
+        topk_accuracies = evaluate_CodeBERT(validation_data_file, model, tokenizer)
+
     print("="*30)
-    print(f"{name}\n{topk_accuracies}")
+    print(f"{model_type}-{value_abstraction}\n{topk_accuracies}")
     print("="*30)
 
 
 if __name__ == "__main__":
+    args = parser.parse_args()
 
-    # evaluate_model("fine-grained",
-    #                "data/codeT5_models/jan5_5_projects/pytorch_model_epoch9.bin",
-    #                "data/codeT5_models/jan5_5_projects/validatejan5_5_projects.pt")
-    
-    evaluate_model("coarse-grained",
-                   "data/codeT5_models/jan5_5_projects_coarse-grained/pytorch_model_epoch9.bin",
-                   "data/codeT5_models/jan5_5_projects_coarse-grained/validatejan5_5_projects_coarse-grained.pt")
+    evaluate_model(args.model_type,
+                   args.value_abstraction,
+                   args.state_file,
+                   args.validate_tensors)
